b454c4e34e8a912186c9375f4b90425a19efc859
jkreller <33465273+jkreller@users.noreply.github.com>
2023-03-03 11:55:15 +0100

    Fix style

--- src/components/MessageChoices.jsx
+++ src/components/MessageChoices.jsx
@@ -3,7 +3,7 @@ import Message from "./Message";
 
 function MessageChoices({ choices, onSelect, isUser }) {
     return (
-      <Stack>
+      <Stack ml='10px' mr='10px'>
         <Text alignSelf={isUser ? 'flex-start' : 'flex-end'}>Select a message:</Text>
         {choices.map((choice, index) => (
           <Message key={index} onClick={() => onSelect(choice)} isUser={isUser}>{choice}</Message>
